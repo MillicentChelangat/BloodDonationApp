68201583c67a57c0558bbdd59c1ac7fee3a91c70
MillicentChelangat <millicentchelangat42@gmail.com>
2025-02-22 20:43:57 +0300

    changes

--- Blood_bank/src/main/java/com/blooddonation/service/DonationService.java
+++ Blood_bank/src/main/java/com/blooddonation/service/DonationService.java
@@ -9,7 +9,6 @@ import java.util.NoSuchElementException;
 
 @Service
 public class DonationService {
-
     private final DonationRepository donationRepository;
 
     public DonationService(DonationRepository donationRepository) {
