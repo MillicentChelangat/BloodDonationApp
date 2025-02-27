3fe2fc8d70617c302c4c657924f647d5aa99cf00
MillicentChelangat <millicentchelangat42@gmail.com>
2025-02-27 09:30:04 +0300

    Added DonationController with CRUD operations

--- Blood_bank/src/main/java/com/blooddonation/BloodDonationBackendApplication.java
+++ Blood_bank/src/main/java/com/blooddonation/BloodDonationBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BloodDonationApplication {
+public class BloodDonationBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BloodDonationApplication.class, args);
+        SpringApplication.run(BloodDonationBackendApplication.class, args);
     }
 }
